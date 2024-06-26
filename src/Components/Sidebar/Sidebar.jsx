7bca37a06d9fcd66cb41256c90d6c9755db1e964
anasrazaweb <anasrazadev@gmail.com>
2024-06-27 00:22:48 +0500

    Sidebar setup

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,37 @@
+import { ListOrdered, PackageSearch, Settings } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
+  const Mennu = [
+    {
+      lable: "Product",
+      icone: <PackageSearch />
+    },
+    {
+      lable: "Setting",
+      icone: <Settings />
+    },
+    {
+      lable: "Order",
+      icone: <ListOrdered />
+    },
+  ]
   return (
-    <div>Sidebar</div>
+    <div className=' h-full w-full'>
+      <div className=' flex flex-col space-x-1 w-full '>
+        {
+          Mennu.map((item, index) => (
+            <Link className=' hover:bg-zinc-50 flex gap-5 w-full py-3 px-5'>
+              {index.icone}
+              <h2 className=' font-medium '> {item.lable}</h2>
+            </Link>
+          ))
+        }
+
+      </div>
+
+    </div>
   )
 }
 
